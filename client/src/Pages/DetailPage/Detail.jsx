73fde758f69923d59545e0a5cc10ba41a114d97c
diegoalparra <diegoalparra08@gmail.com>
2023-08-20 22:05:58 -0500

    detail view works

--- client/src/Pages/DetailPage/Detail.jsx
+++ client/src/Pages/DetailPage/Detail.jsx
@@ -2,24 +2,53 @@ import { useParams } from "react-router-dom"; //permite recibir el
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getDetail } from '../../Redux/actions';
+import NavBar from "../../Components/NavBar/NavBar";
 
 
-function Detail(){
+function Detail() {
 
- const { id } = useParams();
-const dispatch = useDispatch();
-const detailedGame = useSelector((state) => state.gameDetail)
+    const { id } = useParams();
+    const dispatch = useDispatch();
+    const detailedGame = useSelector((state) => state.gameDetail)
 
- useEffect(() => { //useEffect controla el ciclo de vida del componente
-    dispatch(getDetail(id)) //usa el dispatch para hacer el mount de todos los juegos
-}, [dispatch]);
+    useEffect(() => { //useEffect controla el ciclo de vida del componente
+        dispatch(getDetail(id)) //usa el dispatch para hacer el mount de todos los juegos
+    }, [dispatch]);
 
- return(
-    <div>
-       {`detail del juego ${id}`}
-       <h2>{detailedGame.name}</h2>
-    </div>
- )
+    return (
+        <div>
+            <NavBar/>
+
+            {`detail del juego ${id}`}
+            <img src={detailedGame.image && detailedGame.image} alt={detailedGame.name} />
+            <h2>{detailedGame.name}</h2>
+            <h4>Game ID: {detailedGame.id}</h4>
+            <ul>
+                {detailedGame.genres && detailedGame.genres.length > 0 ? (
+                    <li>
+                       <strong>Genres: </strong> {detailedGame.genres.map(genre => genre.name).join(', ')}
+                    </li>
+                ) : (
+                    <li>Genres: Not defined</li>
+                )}
+            </ul>
+
+            <ul>
+                {detailedGame.platforms && detailedGame.platforms.length > 0 ? (
+                    <li>
+                        <strong>Platforms:</strong> {detailedGame.platforms.map(platform => platform.name).join(', ')}
+                    </li>
+                ) : (
+                    <li>Platforms: Not defined</li>
+                )}
+            </ul>
+            <h4>Rating: {detailedGame?.rating}</h4>
+            <h4>Released on: {detailedGame?.released}</h4>
+            <p>{detailedGame?.description}</p>
+
+
+        </div>
+    )
 };
 
 export default Detail;
