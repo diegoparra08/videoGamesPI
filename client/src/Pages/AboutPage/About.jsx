c9ef00b4bb582e274afe6cff019ff2966193687e
diegoalparra <diegoalparra08@gmail.com>
2023-08-29 21:52:42 -0500

    working on the about page

--- client/src/Pages/AboutPage/About.jsx
+++ client/src/Pages/AboutPage/About.jsx
@@ -4,7 +4,15 @@ import { AboutContainerDiv } from './About.styles'
 function About(){
     return (
         <AboutContainerDiv className="about">
-            about!
+            
+            This proyect is the result of the knowledge adquire during my studies in Henry. 
+            I have learned a lot about React, Redux, Express, PostgreSQL and Javascript. 
+
+            It has cards with videogames informrmation and can filter by genre or origin, you can order the results,
+            go to a game detail and even create your own game. I thank Henry and all the mentors an people that helped
+            me get the skills to develop it. I hope you enjoy it!
+
+
         </AboutContainerDiv>
         )
 };
